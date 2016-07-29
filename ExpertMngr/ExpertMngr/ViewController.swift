9b0b69297ad45afd83394f88e4da476bcb2b03a1
poodarchu <poodarchu@gmail.com>
2016-07-29 14:33:46 +0800

    Initialize the project.

---
+++ ExpertMngr/ExpertMngr/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ExpertMngr
+//
+//  Created by P. Chu on 7/29/16.
+//  Copyright © 2016 P. Chu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
