bf49578f70a1e44e21834814485084c03aa7e6a6
poodarchu <poodarchu@gmail.com>
2016-08-17 14:19:45 +0800

    add iOS version of the Expert Recruiting System.

---
+++ ExpertRecruitSystem/ExpertRecruitSystem/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExpertRecruitSystem
+//
+//  Created by P. Chu on 8/17/16.
+//  Copyright © 2016 PDC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
