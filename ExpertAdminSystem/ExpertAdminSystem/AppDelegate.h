a1095f2c2c19a425432b133942abd0090a30eff2
poodarchu <poodarchu@gmail.com>
2016-08-18 22:04:08 +0800

    finally decided to use objective-c and sqlite3

---
+++ ExpertAdminSystem/ExpertAdminSystem/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ExpertAdminSystem
+//
+//  Created by P. Chu on 8/18/16.
+//  Copyright © 2016 PDC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ExpertAdminSystem/ExpertAdminSystem/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ExpertAdminSystem
+//
+//  Created by P. Chu on 8/18/16.
+//  Copyright © 2016 PDC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
